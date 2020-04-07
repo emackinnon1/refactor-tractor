56d12efbe91f7d4058c44c11c06c5b68310e6be4
December G <dgarnetsmith@gmail.com>
2020-04-07 12:54:58 -0600

    Remove extraneous tests

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -3,96 +3,68 @@ import Activity from '../src/Activity';
 import UserRepository from '../src/UserRepository';
 import User from '../src/User';
 
+import userTestData from '../src/data/users-test-data';
+import activityTestData from '../src/data/activity-test-data';
+
 describe('Activity', function() {
-  let activity;
+  let activity1, activity2, activity3;
   let user1, user2, user3;
   let userRepository;
   beforeEach(() => {
-    user1 = new User({
-      'id': 1,
-      'name': 'Luisa Hane',
-      'address': '15195 Nakia Tunnel, Erdmanport VA 19901-1697',
-      'email': 'Diana.Hayes1@hotmail.com',
-      'strideLength': 4.3,
-      'dailyStepGoal': 10000,
-      'friends': [
-        16,
-        4,
-        8
-      ]
-    });
-    user2 = new User({
-      "id": 2,
-      "name": "Jarvis Considine",
-      "address": "30086 Kathryn Port, Ciceroland NE 07273",
-      "email": "Dimitri.Bechtelar11@gmail.com",
-      "strideLength": 4.5,
-      "dailyStepGoal": 2000,
-      "friends": [
-        9,
-        18,
-        24,
-        19
-      ]
-    })
-    userRepository = new UserRepository([user1, user2]);
+
+    user1 = new User(userTestData[0]);
+    user2 = new User(userTestData[1]);
+    user3 = new User(userTestData[2]);
+
+    activity1 = new Activity(activityTestData[0]);
+    activity2 = new Activity(activityTestData[1]);
+    activity3 = new Activity(activityTestData[2]);
+
+    userRepository = new UserRepository([user1, user2, user3]);
     // userRepository.users.push(user1, user2);
-    activity1 = new Activity({
-      "userID": 1,
-      "date": "2019/06/15",
-      "numSteps": 3684,
-      "minutesActive": 140,
-      "flightsOfStairs": 16
-    }, userRepository);
-    activity2 = new Activity({
-      "userID": 2,
-      "date": "2019/06/20",
-      "numSteps": 2856,
-      "minutesActive": 280,
-      "flightsOfStairs": 22
-    }, userRepository);
+    // activity1 = new Activity({
+    //   "userID": 1,
+    //   "date": "2019/06/15",
+    //   "numSteps": 3684,
+    //   "minutesActive": 140,
+    //   "flightsOfStairs": 16
+    // }, userRepository);
+    // activity2 = new Activity({
+    //   "userID": 2,
+    //   "date": "2019/06/20",
+    //   "numSteps": 2856,
+    //   "minutesActive": 280,
+    //   "flightsOfStairs": 22
+    // }, userRepository);
   });
+
   it('should be a function', function() {
     expect(Activity).to.be.a('function');
   });
+
   it('should be an instance of activity', function() {
     expect(activity1).to.be.an.instanceof(Activity);
   });
-  it('should hold a userId', function() {
-    expect(activity2.userId).to.equal(2);
+
+  // it.only('should be able to add activities to user record', function() {
+  //
+  //   activity1.addActivity(userRepository);
+  //
+  //   expect(user1.activityRecord.length).to.equal(1);
+  // });
+
+  // it.only('should have a method that calculate miles walked', function() {
+  //   expect(activity1.calculateMiles(userRepository)).to.equal(3.0);
+  // });
+
+  it('should return false if goal isn\'t met', function() {
+    activity1.compareStepGoal(userRepository);
+    expect(activity1.reachedStepGoal).to.equal(false);
   });
-  it('should hold a date', function() {
-    expect(activity1.date).to.equal("2019/06/15");
+
+  it('should return true if goal is met', function() {
+    activity2.compareStepGoal(userRepository);
+    expect(activity2.reachedStepGoal).to.equal(true);
   });
-  it('should hold number of steps', function() {
-    expect(activity1.steps).to.equal(3684);
+
   });
-  it('should hold minutes active', function() {
-    expect(activity2.minutesActive).to.equal(280);
-  });
-  it('should hold flights of stairs', function() {
-    expect(activity2.flightsOfStairs).to.equal(22);
-  });
-  it('should have a default value of 0 for miles walked', function() {
-    expect(activity2.milesWalked).to.equal(0);
-  });
-  it('should have a default value of null for reached step goal', function() {
-    expect(activity2.reachedStepGoal).to.equal(null);
-  });
-  it('doActivity should add activities to user record', function() {
-    expect(user1.activityRecord.length).to.equal(1);
-  });
-  it('should have a method that calculate miles walked', function() {
-    expect(activity1.calculateMiles(userRepository)).to.equal('3.0');
-  });
-  describe('compareStepGoal', function() {
-    it('should return false if goal isn\'t met', function() {
-      activity1.compareStepGoal(userRepository);
-      expect(activity1.reachedStepGoal).to.equal(false);
-    });
-    it('should return true if goal is met', function() {
-      activity2.compareStepGoal(userRepository);
-      expect(activity2.reachedStepGoal).to.equal(true);
-    });
-  });
-});
