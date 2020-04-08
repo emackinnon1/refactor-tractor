96038cd3088351976d2ef433df98b1cb87160721
December G <dgarnetsmith@gmail.com>
2020-04-08 01:58:37 -0600

    Remove extra methods from Activity class

--- src/Activity.js
+++ src/Activity.js
@@ -13,19 +13,6 @@ class Activity {
 // For a specific day (specified by a date), return the miles a user has walked based on their number of steps (use their strideLength to help calculate this)
 // needs to be tested -->
 
-  calculateMiles(userRepository) {
-    let walkingUser = userRepository.users.find(user => {
-      return user.id === this.userId;
-    });
-    return Number(Math.round(this.numSteps * walkingUser.strideLength / 5280).toFixed(1));
-  }
-
-  compareStepGoal(userRepository) {
-    let userStepGoal = userRepository.users.find(user => {
-      return user.id === this.userId;
-    }).dailyStepGoal;
-    this.reachedStepGoal = this.steps >= userStepGoal;
-  }
 }
 
 export default Activity;
